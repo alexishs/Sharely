30ec3ebe7c60b9e479a5e1db116c13817a22824f
Houdheyfa Talib <houdheyfa.talib@gmail.com>
2019-10-28 14:38:48 +0100

    Update LigneFacture.java

--- src/main/java/fr/dawan/sharely/beans/LigneFacture.java
+++ src/main/java/fr/dawan/sharely/beans/LigneFacture.java
@@ -3,12 +3,13 @@ package fr.dawan.sharely.beans;
 public class LigneFacture {
 
 	private String libelle;
+	private int quantite;
 	private double montant;
 	private Utilisateur utilisateur;
 	
 	public LigneFacture() {}
 
-	public LigneFacture(String libelle, double montant, Utilisateur utilisateur) {
+	public LigneFacture(String libelle, int quantite, double montant, Utilisateur utilisateur) {
 		super();
 		this.libelle = libelle;
 		this.montant = montant;
@@ -23,6 +24,14 @@ public class LigneFacture {
 		this.libelle = libelle;
 	}
 
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	
 	public double getMontant() {
 		return montant;
 	}
@@ -38,5 +47,7 @@ public class LigneFacture {
 	public void setUtilisateur(Utilisateur utilisateur) {
 		this.utilisateur = utilisateur;
 	}
+
+
 	
 }
