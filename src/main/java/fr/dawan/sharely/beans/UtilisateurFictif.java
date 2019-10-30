3cb4ce041341def17df4af9f42945c9856e94acf
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2019-10-30 13:53:50 +0100

    modif classe...

--- src/main/java/fr/dawan/sharely/beans/UtilisateurFictif.java
+++ src/main/java/fr/dawan/sharely/beans/UtilisateurFictif.java
@@ -4,13 +4,12 @@ import java.util.Date;
 
 public class UtilisateurFictif extends Utilisateur {
 
-	private UtilisateurReel createur; // utilisateur ayant crée l'utilisateur fictif
+	private Facture facture;
 	private Date dateFin; // null par défaut, date de fin de vie si création de l'utilisateur réel associé - autodestruction
-	private UtilisateurReel repreneur; // utilisateur ayant repris l'utilisateur fictif
 	
-	public UtilisateurFictif(int id, String nom, String prenom, String email, UtilisateurReel createur) {
+	public UtilisateurFictif(int id, String nom, String prenom, String email, Facture facture) {
 		super(id, nom, prenom, email);
-		this.createur = createur;
+		this.facture = facture;
 	}
 
 	public Date getDateFin() {
@@ -20,18 +19,5 @@ public class UtilisateurFictif extends Utilisateur {
 	public void setDateFin(Date dateFin) {
 		this.dateFin = dateFin;
 	}
-
-	public UtilisateurReel getRepreneur() {
-		return repreneur;
-	}
-
-	public void setRepreneur(UtilisateurReel repreneur) {
-		this.repreneur = repreneur;
-	}
-
-	public UtilisateurReel getCreateur() {
-		return createur;
-	}
-
 
 }
