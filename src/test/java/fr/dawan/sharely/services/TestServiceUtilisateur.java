7fb320f6e122a28e345d210d8383824dd09e9ba6
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2019-12-06 20:17:17 +0100

    Création TestServiceUtilisateur

---
+++ src/test/java/fr/dawan/sharely/services/TestServiceUtilisateur.java
@@ -0,0 +1,49 @@
+package fr.dawan.sharely.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestServiceUtilisateur {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDemandeInscriptionUtilisateur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidationInscriptionUtilisateur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConnexion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnvoiEmailRecuperationMotDePasse() {
+		fail("Not yet implemented");
+	}
+
+}
